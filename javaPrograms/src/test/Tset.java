5db7c166bc740724872b7f5683d381a53713f569
sadiqshaik <31308792+sadiqshaik@users.noreply.github.com>
2019-02-24 21:36:04 +0530

    added sop

--- javaPrograms/src/test/Tset.java
+++ javaPrograms/src/test/Tset.java
@@ -4,7 +4,8 @@ public class Tset {
 	
 	public static void main(String[] args) {
 	
-		System.out.print("i love my parents");
+		System.out.println("i love my parents");
+		System.out.print("i love my wife");
 	}
 	
 	
